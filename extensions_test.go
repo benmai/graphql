0e2df6c22d7b33d432d1aeef90fe32788347c5d5
Ben Isaacs <bisaacs@fleetsmithhq.com>
2019-05-27 21:25:25 -0700

    add test for ResolveFieldDidStart

--- extensions_test.go
+++ extensions_test.go
@@ -5,10 +5,15 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"sort"
+	"sync"
 	"testing"
+
+	"github.com/matryer/is"
 
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/graphql/gqlerrors"
+	"github.com/graphql-go/graphql/language/location"
 	"github.com/graphql-go/graphql/testutil"
 )
 
@@ -33,7 +38,7 @@ func tinit(t *testing.T) graphql.Schema {
 }
 
 func TestExtensionInitPanic(t *testing.T) {
-	ext := newtestExt("testExt")
+	ext := newTestExt()
 	ext.initFn = func(ctx context.Context, p *graphql.Params) context.Context {
 		if true {
 			panic(errors.New("test error"))
@@ -62,7 +67,7 @@ func TestExtensionInitPanic(t *testing.T) {
 }
 
 func TestExtensionParseDidStartPanic(t *testing.T) {
-	ext := newtestExt("testExt")
+	ext := newTestExt()
 	ext.parseDidStartFn = func(ctx context.Context) (context.Context, graphql.ParseFinishFunc) {
 		if true {
 			panic(errors.New("test error"))
@@ -93,7 +98,7 @@ func TestExtensionParseDidStartPanic(t *testing.T) {
 }
 
 func TestExtensionParseFinishFuncPanic(t *testing.T) {
-	ext := newtestExt("testExt")
+	ext := newTestExt()
 	ext.parseDidStartFn = func(ctx context.Context) (context.Context, graphql.ParseFinishFunc) {
 		return ctx, func(err error) {
 			panic(errors.New("test error"))
@@ -121,7 +126,7 @@ func TestExtensionParseFinishFuncPanic(t *testing.T) {
 }
 
 func TestExtensionValidationDidStartPanic(t *testing.T) {
-	ext := newtestExt("testExt")
+	ext := newTestExt()
 	ext.validationDidStartFn = func(ctx context.Context) (context.Context, graphql.ValidationFinishFunc) {
 		if true {
 			panic(errors.New("test error"))
@@ -152,7 +157,7 @@ func TestExtensionValidationDidStartPanic(t *testing.T) {
 }
 
 func TestExtensionValidationFinishFuncPanic(t *testing.T) {
-	ext := newtestExt("testExt")
+	ext := newTestExt()
 	ext.validationDidStartFn = func(ctx context.Context) (context.Context, graphql.ValidationFinishFunc) {
 		return ctx, func([]gqlerrors.FormattedError) {
 			panic(errors.New("test error"))
@@ -180,7 +185,7 @@ func TestExtensionValidationFinishFuncPanic(t *testing.T) {
 }
 
 func TestExtensionExecutionDidStartPanic(t *testing.T) {
-	ext := newtestExt("testExt")
+	ext := newTestExt()
 	ext.executionDidStartFn = func(ctx context.Context) (context.Context, graphql.ExecutionFinishFunc) {
 		if true {
 			panic(errors.New("test error"))
@@ -211,7 +216,7 @@ func TestExtensionExecutionDidStartPanic(t *testing.T) {
 }
 
 func TestExtensionExecutionFinishFuncPanic(t *testing.T) {
-	ext := newtestExt("testExt")
+	ext := newTestExt()
 	ext.executionDidStartFn = func(ctx context.Context) (context.Context, graphql.ExecutionFinishFunc) {
 		return ctx, func(r *graphql.Result) {
 			panic(errors.New("test error"))
@@ -242,7 +247,7 @@ func TestExtensionExecutionFinishFuncPanic(t *testing.T) {
 }
 
 func TestExtensionResolveFieldDidStartPanic(t *testing.T) {
-	ext := newtestExt("testExt")
+	ext := newTestExt()
 	ext.resolveFieldDidStartFn = func(ctx context.Context, i *graphql.ResolveInfo) (context.Context, graphql.ResolveFieldFinishFunc) {
 		if true {
 			panic(errors.New("test error"))
@@ -275,8 +280,166 @@ func TestExtensionResolveFieldDidStartPanic(t *testing.T) {
 	}
 }
 
+func TestExtensionResolveFieldFinish(t *testing.T) {
+	ext := newExtensionMock()
+
+	type resolveFieldFinishCall struct {
+		fieldName string
+		value     interface{}
+		err       error
+	}
+	var mockResolveFieldFinishFuncCalls []resolveFieldFinishCall
+	lockMockResolveFieldFinishFuncCalls := &sync.Mutex{}
+
+	ext.ResolveFieldDidStartFunc = func(ctx context.Context, i *graphql.ResolveInfo) (context.Context, graphql.ResolveFieldFinishFunc) {
+		return ctx, func(value interface{}, err error) {
+			lockMockResolveFieldFinishFuncCalls.Lock()
+			mockResolveFieldFinishFuncCalls = append(
+				mockResolveFieldFinishFuncCalls,
+				resolveFieldFinishCall{i.FieldName, value, err},
+			)
+			lockMockResolveFieldFinishFuncCalls.Unlock()
+		}
+
+	}
+
+	schema, err := graphql.NewSchema(graphql.SchemaConfig{
+		Query: graphql.NewObject(graphql.ObjectConfig{
+			Name: "Type",
+			Fields: graphql.Fields{
+				"asyncValue": &graphql.Field{
+					Type: graphql.String,
+					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+						return func() (interface{}, error) {
+							return "asyncValue value", nil
+						}, nil
+					},
+				},
+				"syncValue": &graphql.Field{
+					Type: graphql.String,
+					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+						return "syncValue value", nil
+					},
+				},
+				"asyncValueError": &graphql.Field{
+					Type: graphql.String,
+					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+						return func() (interface{}, error) {
+							return nil, errors.New("asyncValueError error")
+						}, nil
+					},
+				},
+				"syncValueError": &graphql.Field{
+					Type: graphql.String,
+					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+						return nil, errors.New("syncValueError error")
+					},
+				},
+			},
+		}),
+	})
+	if err != nil {
+		t.Fatalf("Error in schema %v", err.Error())
+	}
+
+	query := `query Example {
+		syncValue
+		asyncValue
+		syncValueError
+		asyncValueError
+	}`
+	schema.AddExtensions(ext)
+
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+
+	syncValueError := gqlerrors.NewFormattedError("syncValueError error")
+	syncValueError.Message = "syncValueError error"
+	syncValueError.Locations = []location.SourceLocation{
+		{
+			Line:   4,
+			Column: 3,
+		},
+	}
+	syncValueError.Path = []interface{}{"syncValueError"}
+
+	asyncValueError := gqlerrors.NewFormattedError("asyncValueError error")
+	asyncValueError.Message = "asyncValueError error"
+	asyncValueError.Locations = []location.SourceLocation{
+		{
+			Line:   5,
+			Column: 3,
+		},
+	}
+	asyncValueError.Path = []interface{}{"asyncValueError"}
+
+	expected := &graphql.Result{
+		Data: map[string]interface{}{
+			"syncValue":       "syncValue value",
+			"asyncValue":      "asyncValue value",
+			"syncValueError":  nil,
+			"asyncValueError": nil,
+		},
+		Errors: []gqlerrors.FormattedError{
+			syncValueError,
+			asyncValueError,
+		},
+	}
+
+	// Check value return.
+	is := is.New(t)
+	is.Equal(expected.Data, result.Data)
+	is.True(testutil.EqualFormattedErrors(expected.Errors, result.Errors))
+
+	// Check that mock was called with the right arguments.
+	calls := ext.ResolveFieldDidStartCalls()
+	is.Equal(4, len(calls))
+	for _, fieldName := range []string{
+		"syncValueError",
+		"asyncValueError",
+		"syncValue",
+		"asyncValue",
+	} {
+		var found bool
+		for _, call := range calls {
+			if call.In2.FieldName == fieldName {
+				found = true
+			}
+		}
+		is.True(found)
+	}
+
+	// TODO test calls to the returned function
+	sort.Slice(mockResolveFieldFinishFuncCalls, func(i, j int) bool {
+		return mockResolveFieldFinishFuncCalls[i].fieldName < mockResolveFieldFinishFuncCalls[j].fieldName
+	})
+
+	expectedCalls := []resolveFieldFinishCall{
+		{
+			fieldName: "asyncValue",
+		},
+		{
+			fieldName: "asyncValueError",
+		},
+		{
+			fieldName: "syncValue",
+		},
+		{
+			fieldName: "syncValueError",
+		},
+	}
+
+	is.Equal(len(expectedCalls), len(mockResolveFieldFinishFuncCalls))
+	for i, c := range mockResolveFieldFinishFuncCalls {
+		expectedCall := expectedCalls[i]
+		is.Equal(expectedCall, c)
+	}
+}
+
 func TestExtensionResolveFieldFinishFuncPanic(t *testing.T) {
-	ext := newtestExt("testExt")
+	ext := newTestExt()
 	ext.resolveFieldDidStartFn = func(ctx context.Context, i *graphql.ResolveInfo) (context.Context, graphql.ResolveFieldFinishFunc) {
 		return ctx, func(v interface{}, err error) {
 			panic(errors.New("test error"))
@@ -307,7 +470,7 @@ func TestExtensionResolveFieldFinishFuncPanic(t *testing.T) {
 }
 
 func TestExtensionGetResultPanic(t *testing.T) {
-	ext := newtestExt("testExt")
+	ext := newTestExt()
 	ext.getResultFn = func(context.Context) interface{} {
 		if true {
 			panic(errors.New("test error"))
@@ -342,54 +505,60 @@ func TestExtensionGetResultPanic(t *testing.T) {
 	}
 }
 
-func newtestExt(name string) *testExt {
-	ext := &testExt{
-		name: name,
-	}
-	if ext.initFn == nil {
-		ext.initFn = func(ctx context.Context, p *graphql.Params) context.Context {
+func newExtensionMock() *graphql.ExtensionMock {
+	return &graphql.ExtensionMock{
+		InitFunc: func(ctx context.Context, p *graphql.Params) context.Context {
 			return ctx
-		}
-	}
-	if ext.parseDidStartFn == nil {
-		ext.parseDidStartFn = func(ctx context.Context) (context.Context, graphql.ParseFinishFunc) {
-			return ctx, func(err error) {
-
-			}
-		}
-	}
-	if ext.validationDidStartFn == nil {
-		ext.validationDidStartFn = func(ctx context.Context) (context.Context, graphql.ValidationFinishFunc) {
-			return ctx, func([]gqlerrors.FormattedError) {
-
-			}
-		}
-	}
-	if ext.executionDidStartFn == nil {
-		ext.executionDidStartFn = func(ctx context.Context) (context.Context, graphql.ExecutionFinishFunc) {
-			return ctx, func(r *graphql.Result) {
-
-			}
-		}
-	}
-	if ext.resolveFieldDidStartFn == nil {
-		ext.resolveFieldDidStartFn = func(ctx context.Context, i *graphql.ResolveInfo) (context.Context, graphql.ResolveFieldFinishFunc) {
-			return ctx, func(v interface{}, err error) {
-
-			}
-		}
-	}
-	if ext.hasResultFn == nil {
-		ext.hasResultFn = func() bool {
+		},
+		NameFunc: func() string {
+			return "mockExtension"
+		},
+		ParseDidStartFunc: func(ctx context.Context) (context.Context, graphql.ParseFinishFunc) {
+			return ctx, func(error) {}
+		},
+		ValidationDidStartFunc: func(ctx context.Context) (context.Context, graphql.ValidationFinishFunc) {
+			return ctx, func([]gqlerrors.FormattedError) {}
+		},
+		ExecutionDidStartFunc: func(ctx context.Context) (context.Context, graphql.ExecutionFinishFunc) {
+			return ctx, func(*graphql.Result) {}
+		},
+		ResolveFieldDidStartFunc: func(ctx context.Context, i *graphql.ResolveInfo) (context.Context, graphql.ResolveFieldFinishFunc) {
+			return ctx, func(interface{}, error) {}
+		},
+		HasResultFunc: func() bool {
 			return false
-		}
-	}
-	if ext.getResultFn == nil {
-		ext.getResultFn = func(context.Context) interface{} {
+		},
+		GetResultFunc: func(ctx context.Context) interface{} {
 			return nil
-		}
+		},
 	}
-	return ext
+}
+
+func newTestExt() *testExt {
+	return &testExt{
+		name: "newTextExt",
+		initFn: func(ctx context.Context, p *graphql.Params) context.Context {
+			return ctx
+		},
+		parseDidStartFn: func(ctx context.Context) (context.Context, graphql.ParseFinishFunc) {
+			return ctx, func(err error) {}
+		},
+		validationDidStartFn: func(ctx context.Context) (context.Context, graphql.ValidationFinishFunc) {
+			return ctx, func([]gqlerrors.FormattedError) {}
+		},
+		executionDidStartFn: func(ctx context.Context) (context.Context, graphql.ExecutionFinishFunc) {
+			return ctx, func(r *graphql.Result) {}
+		},
+		resolveFieldDidStartFn: func(ctx context.Context, i *graphql.ResolveInfo) (context.Context, graphql.ResolveFieldFinishFunc) {
+			return ctx, func(v interface{}, err error) {}
+		},
+		hasResultFn: func() bool {
+			return false
+		},
+		getResultFn: func(context.Context) interface{} {
+			return nil
+		},
+	}
 }
 
 type testExt struct {
--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module github.com/graphql-go/graphql
+
+go 1.12
+
+require github.com/matryer/is v1.2.0
